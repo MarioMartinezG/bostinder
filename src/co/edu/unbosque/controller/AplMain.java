d5d74842c9f2a3c9873e7189e32a0b478709e6d4
Dayana Serrano <jdserrano@unbosque.edu.co>
2022-11-16 20:25:30 -0500

    Commit inicial del proyecto

---
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -0,0 +1,20 @@
+/**
+ * Clase principal de la aplicacion - Ejecuta el metodo main
+ */
+package co.edu.unbosque.controller;
+
+/**
+ * @author Dayana Serrano
+ *
+ */
+public class AplMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainController controller = new MainController();
+
+	}
+
+}
---
+++ src/co/edu/unbosque/controller/MainController.java
@@ -0,0 +1,19 @@
+/**
+ * Controlador principal de la aplicacion
+ */
+package co.edu.unbosque.controller;
+
+/**
+ * @author Dayana Serrano
+ *
+ */
+public class MainController {
+	public MainController() {
+		ejecutar();
+	}
+
+	private void ejecutar() {
+		System.out.println("Ejecutando la aplicacion BosTinder");
+		
+	}
+}
