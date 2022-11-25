f024dd40bb778d60953455c875ccef22441be1d6
achury-m <miguelachury@hotmail.com>
2022-11-25 09:36:21 -0500

    Creacion de vistas

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -3,18 +3,39 @@
  */
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.*;
+
 /**
  * @author Dayana Serrano
  *
  */
 public class AplMain {
+	MainController controller;
+	
+	MainView mainView;
+	HomeView homeView;
+	IndexView indexView;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MainController controller = new MainController();
-
+		new AplMain().start();
 	}
-
+	public void start() {
+		initializeView();
+		initializeControllers();
+	}
+	
+	public void initializeView() {
+		indexView = new IndexView();
+		mainView = new MainView();
+	}
+	public void initializeControllers() {
+		controller = new MainController();
+	}
+	public void setViewsToControllers() {
+		controller.setMainView(mainView);
+		controller.setIndexView(indexView);
+	}
 }
--- src/co/edu/unbosque/view/MainView.java
+++ src/co/edu/unbosque/view/MainView.java
@@ -1,11 +1,67 @@
 package co.edu.unbosque.view;
 
-public class MainView {
+import java.awt.Color;
 
-	public static void main(String[] args) {
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class MainView extends JFrame {
+
+	RegisterView registerView;
+	IndexView indexView;
+	LoginView loginView;
+	HomeView homeView;
+
+	public MainView() {
+
+		setTitle("Pantalla Principal | AppName");
+		setSize(1300, 650);
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		intializeComponents();
+
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	public void intializeComponents() {
+		addIndexPanel();
+		addRegisterPanel();
+		addLoginPanel();
+		addHomePanel();
+	}
+	
+	public void addIndexPanel() {
+		indexView = new IndexView();
+		indexView.setBounds(0, 0, 1290, 650);
+		indexView.setLayout(null);
+		getContentPane().add(indexView);
+		indexView.setVisible(true);
+	}
+
+	public void addRegisterPanel() {
+		registerView = new RegisterView();
+		registerView.setBounds(0, 0, 1290, 650);
+		registerView.setLayout(null);
+		getContentPane().add(registerView);
+		registerView.setVisible(false);
 		
-		System.out.println("programando con el mariesito");
-
+	}
+	public void addLoginPanel() {
+		loginView = new LoginView();
+		loginView.setBounds(0, 0, 1290, 650);
+		loginView.setLayout(null);
+		getContentPane().add(loginView);
+		loginView.setVisible(false);
+	}
+	public void addHomePanel() {
+		homeView = new HomeView();
+		homeView.setBounds(0, 0, 1290, 650);
+		homeView.setLayout(null);
+		getContentPane().add(homeView);
+		homeView.setVisible(false);
 	}
 
 }
