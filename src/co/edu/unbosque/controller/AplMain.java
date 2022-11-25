7444ca4e15fe7ef9b5df6a9b5db36116bafbc149
andres.martinez <andres.martinez@macroproyectos.com>
2022-11-25 09:58:04 -0500

    Solucion conflictos vistas

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -3,16 +3,30 @@
  */
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.*;
+
 /**
  * @author Dayana Serrano
  *
  */
 public class AplMain {
+	MainController controller;
+	
+	MainView mainView;
+	HomeView homeView;
+	IndexView indexView;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MainController controller = new MainController();
+		new AplMain().start();
+	}
+	public void start() {
+		initializeControllers();
+	}
+
+	public void initializeControllers() {
+		controller = new MainController();
 	}
 }
--- src/co/edu/unbosque/controller/MainController.java
+++ src/co/edu/unbosque/controller/MainController.java
@@ -1,19 +1,99 @@
 /**
-  * Controlador principal de la aplicacion
+* Controlador principal de la aplicacion
  */
 package co.edu.unbosque.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import co.edu.unbosque.view.*;
+
 /**
- * @author Dayana Serrano
+ * @author Dayana Serrano, Mario Martínez, Daniela Murcia, Miguel Sánchez
  *
  */
-public class MainController {
+public class MainController implements ActionListener {
+
+	MainView mainView;
+	IndexView indexView;
+	LoginView loginView;
+	HomeView homeView;
+	RegisterView registerView;
+
 	public MainController() {
+
+		initializeViews();
 		ejecutar();
 	}
 
+	public void initializeViews() {
+		mainView = new MainView();
+		indexView = new IndexView();
+		loginView = new LoginView();
+	}
+
 	private void ejecutar() {
-		System.out.println("Ejecutando la aplicacion BosTinder");
+		listeners();
+	}
+
+	public void listeners() {
+		indexViewListeners();
+	}
+
+	public void indexViewListeners() {
+		indexView.getRegisterBtn().addActionListener(this);
+		indexView.getLoginBtn().addActionListener(this);
+	}
+
+	public void mainViewListeners() {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String event = e.getActionCommand();
+		System.out.println(event);
+		
 		
 	}
+
+	public MainView getMainView() {
+		return mainView;
+	}
+
+	public void setMainView(MainView mainView) {
+		this.mainView = mainView;
+	}
+
+	public IndexView getIndexView() {
+		return indexView;
+	}
+
+	public void setIndexView(IndexView indexView) {
+		this.indexView = indexView;
+	}
+
+	public LoginView getLoginView() {
+		return loginView;
+	}
+
+	public void setLoginView(LoginView loginView) {
+		this.loginView = loginView;
+	}
+
+	public HomeView getHomeView() {
+		return homeView;
+	}
+
+	public void setHomeView(HomeView homeView) {
+		this.homeView = homeView;
+	}
+
+	public RegisterView getRegisterView() {
+		return registerView;
+	}
+
+	public void setRegisterView(RegisterView registerView) {
+		this.registerView = registerView;
+	}
+
 }
